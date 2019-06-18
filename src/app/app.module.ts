0c5e460712af6ee389738131f325f6f5e6dd2955
Overflow404 <gaspare.armato1@gmail.com>
2019-06-18 19:55:48 +0200

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarHeaderComponent } from './calendar-header/calendar-header.component';
+import { CalendarBodyComponent } from './calendar-body/calendar-body.component';
+import { CalendarTrailerComponent } from './calendar-trailer/calendar-trailer.component';
+import { CalendarBodyCellComponent } from './calendar-body-cell/calendar-body-cell.component';
+import {MatButtonModule, MatCardModule, MatGridListModule, MatInputModule, MatToolbarModule} from '@angular/material/';
+import {DataService} from './data.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    CalendarHeaderComponent,
+    CalendarBodyComponent,
+    CalendarTrailerComponent,
+    CalendarBodyCellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
