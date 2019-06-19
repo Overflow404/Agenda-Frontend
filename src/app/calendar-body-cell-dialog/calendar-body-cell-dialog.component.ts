c9e786c552457d2d57e2ae5ce9675d2d26c967af
Overflow404 <gaspare.armato1@gmail.com>
2019-06-19 13:03:32 +0200

    Added mandatory fields.

--- src/app/calendar-body-cell-dialog/calendar-body-cell-dialog.component.ts
+++ src/app/calendar-body-cell-dialog/calendar-body-cell-dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component,  OnInit} from '@angular/core';
 import {DateManager} from '../date/DateManager';
+import {NgxMaterialTimepickerComponent} from 'ngx-material-timepicker';
 
 @Component({
   selector: 'app-calendar-body-cell-dialog',
@@ -10,6 +11,9 @@ export class CalendarBodyCellDialogComponent implements OnInit {
 
   date: Date;
   disableDatePickers = false;
+  subject: string;
+  startDate: string;
+  endDate: string;
 
   constructor(private dateManager: DateManager) { }
 
@@ -18,5 +22,16 @@ export class CalendarBodyCellDialogComponent implements OnInit {
 
   onSlideChange() {
     this.disableDatePickers = !this.disableDatePickers;
+  }
+
+  onStartDateSelected(startTime: NgxMaterialTimepickerComponent) {
+    const startDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(),
+      startTime.selectedHour.time, startTime.selectedMinute.time);
+    alert(startDate.toJSON());
+  }
+
+  onEndDateSelected(endTime: NgxMaterialTimepickerComponent) {
+    const endDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(),
+      endTime.selectedHour.time, endTime.selectedMinute.time);
   }
 }
