3ead31011da07e801ad6f9609232bb7347259594
Overflow404 <gaspare.armato1@gmail.com>
2019-06-19 11:11:11 +0200

    Cell component now show a booking dialog.

--- src/app/calendar-body-cell/calendar-body-cell.component.ts
+++ src/app/calendar-body-cell/calendar-body-cell.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DateManager} from '../date/DateManager';
+import {MatDialog} from '@angular/material';
+import {CalendarBodyCellDialogComponent} from '../calendar-body-cell-dialog/calendar-body-cell-dialog.component';
 
 @Component({
   selector: 'app-calendar-body-cell',
@@ -10,8 +12,8 @@ export class CalendarBodyCellComponent implements OnInit {
 
   @Input() date: Date;
 
-
-  constructor(private dateManager: DateManager) {
+  constructor(private dateManager: DateManager,
+              public dialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -22,4 +24,13 @@ export class CalendarBodyCellComponent implements OnInit {
     return this.dateManager.getWeekdayNameByDay(day);
   }
 
+  doubleClickCallback() {
+    const dialogRef = this.dialog.open(CalendarBodyCellDialogComponent);
+    const instance = dialogRef.componentInstance;
+    instance.date = this.date;
+
+    dialogRef.afterClosed().subscribe(result => {
+      /*alert('Closed');*/
+    });
+  }
 }
